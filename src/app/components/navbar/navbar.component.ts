c85c484ace989d119f73959f7ceadfec94c2d7c1
BernardoBiazzi <bernardo.biazzi@gmail.com>
2022-10-31 19:01:23 -0300

    inclusão user na navbar + dark theme

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialUser } from 'angularx-social-login';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public profilePicture: string = '';
+  public user: SocialUser | undefined;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.setUserAndPicture();
+    this.subscribeToAuthStateChanged();
+  }
+
+  public goToUserProfile() {
+    this.router.navigate(['./login']);
+  }
+
+  private setUserAndPicture() {
+    this.user = this.authService.user;
+    this.profilePicture = this.authService.profilePicture;
+  }
+
+  private subscribeToAuthStateChanged() {
+    this.authService.authStateChanged.subscribe(() => {
+      this.setUserAndPicture();
+    });
   }
 
 }
