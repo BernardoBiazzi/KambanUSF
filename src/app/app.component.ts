dc6b03dc63a6bd3b29ddf6b7d933551c02ac7268
BernardoBiazzi <bernardo.biazzi@gmail.com>
2022-11-01 00:52:17 -0300

    switchTheme button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,35 @@ export class AppComponent {
   ngOnInit() {
     this.router.navigate(['/login']);
   }
+
+  get currentTheme(): string {
+    if (this.isLightTheme) return 'Claro';
+    else return 'Escuro';
+  }
+
+  get isLightTheme(): boolean {
+    return document.body.classList.contains('light');
+  }
+
+  get isDarkTheme(): boolean {
+    return document.body.classList.contains('dark');
+  }
+
+  switchTheme(): void {
+
+    if (document.body.classList.contains('light')) {
+
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+      document.querySelector("meta[name='theme-color']")?.setAttribute('content', '#252525');
+
+    } else {
+
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+      document.querySelector("meta[name='theme-color']")?.setAttribute('content', '#ffffff');
+
+    }
+
+  }
 }
